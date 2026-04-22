cbf8a7f886ac06ac2c4b1607738da778e6996a74
borek707 <borek707@gmail.com>
2026-04-22 09:35:38 +0200

    refactor: update platform exports

--- src/council/platform/__init__.py
+++ src/council/platform/__init__.py
@@ -2,10 +2,18 @@ from __future__ import annotations
 
 from council.platform.base import PlatformAdapter
 from council.platform.cli_adapter import CLIAdapter
+from council.platform.cursor_adapter import CursorAdapter
+from council.platform.copilot_adapter import GitHubCopilotAdapter
+from council.platform.idx_adapter import GoogleIDXAdapter
 from council.platform.kimi_adapter import KimiAdapter
+from council.platform.web_adapter import WebPlatformAdapter
 
 __all__ = [
     "PlatformAdapter",
     "CLIAdapter",
+    "CursorAdapter",
+    "GitHubCopilotAdapter",
+    "GoogleIDXAdapter",
     "KimiAdapter",
+    "WebPlatformAdapter",
 ]
